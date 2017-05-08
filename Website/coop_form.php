656fdbd15a946a75fe7eec34b76592abc6626e75
Daniel Roberts <djr9478@rit.edu>
2017-05-08 14:47:29 -0400

    Fixed styling issue

--- Website/coop_form.php
+++ Website/coop_form.php
@@ -91,19 +91,13 @@
 
 <body onload="loadFromStorage()">
 
-<?php
-//get and set company and student ID
-echo '<input id="studentID" type="hidden" value="'.$_GET['studentID'].'/>';
-echo '<input id="companyID" type="hidden" value="'.$_GET['companyID'].'/>';
-?>
-
 <div class="container">
 	<h1>Student Coop Evaluation</h1>
 	<form method="post" action="functions.php?function=submitStudentForm">
 	<?php
 	//get and set company and student ID
-	echo '<input id="studentID" name="studentID" type="hidden" value="'.$_GET['studentID'].'>';
-	echo '<input id="companyID" name="companyID" type="hidden" value="'.$_GET['companyID'].'>';
+	echo '<input id="studentID" name="studentID" type="hidden" value="'.$_GET['studentID'].'/>';
+	echo '<input id="companyID" name="companyID" type="hidden" value="'.$_GET['companyID'].'/>';
 	?>
 	<div class="form-group">
 		<label for="name">Name</label>
