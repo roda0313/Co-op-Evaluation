5bc4d0eca87ee03caeee6266a302270e2d278490
Daniel Roberts <djr9478@rit.edu>
2017-05-09 01:27:39 -0400

    Made evaluation forms load from the API

--- Website/coop_form.php
+++ Website/coop_form.php
@@ -25,71 +25,108 @@
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
   
 	<script>
-		function loadFromStorage()
-		{
-			document.getElementById("name").value = localStorage.getItem("name")
-			document.getElementById("email").value = localStorage.getItem("email");
-			document.getElementById("ename").value = localStorage.getItem("ename");
-			document.getElementById("eemail").value = localStorage.getItem("eemail");
-			document.getElementById("position").value = localStorage.getItem("position");
-			//document.getElementById("q1").value = localStorage.getItem("q1");
-			document.getElementById("q2").value = localStorage.getItem("q2");
-			document.getElementById("q3").value = localStorage.getItem("q3");
-			//document.getElementById("q4").value = localStorage.getItem("q4");
-			document.getElementById("q5").value = localStorage.getItem("q5");
-		}
+	function getStorageFromAPI()
+	{
+		var sid = document.getElementById("studentID")
+		var cid = document.getElementById("companyID");
+		
+		var url = "http://vm344f.se.rit.edu/API/API.php?team=coop_eval&function=getStudentEvaluation&studentID=" + sid + "&companyID=" + cid;
 	
-		function onFormChange()
-		{	
-			var input = document.getElementById("name");
-			localStorage.setItem("name", input.value)
-		
-			input = document.getElementById("email");
-			localStorage.setItem("email", input.value);
-			
-			input = document.getElementById("ename");
-			localStorage.setItem("ename", input.value);
-			
-			input = document.getElementById("eemail");
-			localStorage.setItem("eemail", input.value);
-			
-			input = document.getElementById("position");
-			localStorage.setItem("position", input.value);
-			
-			//input = document.getElementById("q1");
-			//localStorage.setItem("q1", input.value);
-			
-			input = document.getElementById("q2");
-			localStorage.setItem("q2", input.value);
-			
-			input = document.getElementById("q3");
-			localStorage.setItem("q3", input.value);
-			
-			//input = document.getElementById("q4");
-			//localStorage.setItem("q4", input.value);
-			
-			input = document.getElementById("q5");
-			localStorage.setItem("q5", input.value);
-			
-		}
-		function saveForm()
-		{
-			$url = "http://vm344f.se.rit.edu/Website/functions.php?function=submitStudentForm&save=true";
-		
-			var xmlHttp = new XMLHttpRequest();
-			xmlHttp.onreadystatechange = function() {
-				if (this.readyState == 4 && this.status == 200) {
-					console.log(xmlHttp.responseText);
+		var xmlHttp = new XMLHttpRequest();
+		xmlHttp.onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200) {
+				var data = JSON.parse(xmlHttp.responseText);
+				if (data.length > 0)
+				{					
+					localStorage.setItem("name", data[0].name)
+					localStorage.setItem("email", data[0].email);
+					localStorage.setItem("ename", data[0].sname);
+					localStorage.setItem("eemail", data[0].semail);
+					localStorage.setItem("position", data[0].position);
+					localStorage.setItem("q1", data[0].q1);
+					localStorage.setItem("q2", data[0].q2);
+					localStorage.setItem("q3", data[0].q3);
+					localStorage.setItem("q4", data[0].q4);
+					localStorage.setItem("q5", data[0].q5);
 				}
-			};
-			
-			xmlHttp.open( "POST", $url, true ); // false for synchronous request
-			xmlHttp.send();
-		}
+			}
+		};
+		
+		xmlHttp.open( "GET", url, true ); // false for synchronous request
+		xmlHttp.send();
+	}
+	function loadFromStorage()
+	{
+		getStorageFromAPI();
+		
+		document.getElementById("name").value = localStorage.getItem("name")
+		document.getElementById("email").value = localStorage.getItem("email");
+		document.getElementById("ename").value = localStorage.getItem("ename");
+		document.getElementById("eemail").value = localStorage.getItem("eemail");
+		document.getElementById("position").value = localStorage.getItem("position");
+		document.getElementById("q1").value = localStorage.getItem("q1");
+		document.getElementById("q2").value = localStorage.getItem("q2");
+		document.getElementById("q3").value = localStorage.getItem("q3");
+		document.getElementById("q4").value = localStorage.getItem("q4");
+		document.getElementById("q5").value = localStorage.getItem("q5");
+	}
+
+	function onFormChange()
+	{	
+		var input = document.getElementById("name");
+		localStorage.setItem("name", input.value)
+	
+		input = document.getElementById("email");
+		localStorage.setItem("email", input.value);
+		
+		input = document.getElementById("ename");
+		localStorage.setItem("ename", input.value);
+		
+		input = document.getElementById("eemail");
+		localStorage.setItem("eemail", input.value);
+		
+		input = document.getElementById("position");
+		localStorage.setItem("position", input.value);
+		
+		//input = document.getElementById("q1");
+		//localStorage.setItem("q1", input.value);
+		
+		input = document.getElementById("q2");
+		localStorage.setItem("q2", input.value);
+		
+		input = document.getElementById("q3");
+		localStorage.setItem("q3", input.value);
+		
+		//input = document.getElementById("q4");
+		//localStorage.setItem("q4", input.value);
+		
+		input = document.getElementById("q5");
+		localStorage.setItem("q5", input.value);
+		
+	}
+	function saveForm()
+	{
+		$url = "http://vm344f.se.rit.edu/Website/functions.php?function=submitStudentForm&save=true";
+	
+		var xmlHttp = new XMLHttpRequest();
+		xmlHttp.onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200) {
+				console.log(xmlHttp.responseText);
+			}
+		};
+		
+		xmlHttp.open( "POST", $url, true ); // false for synchronous request
+		xmlHttp.send();
+	}
+	
+	function clearStorage()
+	{
+		localStorage.clear();
+	}
 	</script>
 </head>
 
-<body onload="loadFromStorage()">
+<body onload="loadFromStorage()" onunload="clearStorage()">
 
 <div class="container">
 	<h1>Student Coop Evaluation</h1>
